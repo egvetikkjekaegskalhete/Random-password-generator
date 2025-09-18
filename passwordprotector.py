dee6e0fc49ab97996be3b010b0fb02a5801df808
egvetikkjekaegskalhete <gabrielaasen@gmail.com>
2025-09-18 12:33:05 +0200

    Skrevet ferdig koden for å hashe et passord

--- passwordprotector.py
+++ passwordprotector.py
@@ -1,10 +1,23 @@
 # Password Protector - startkode
-import hashlib
+import random
+import string
 
 # Funksjon som skal hashe et passord
 def hash_password(password):
-    # fullfør koden her
-    return ??? #fullfør koden her
+    
+    upper = random.choices(string.ascii_uppercase, k=2)
+    lower = random.choices(string.ascii_lowercase, k=2)
+    digits = random.choices(string.digits, k=2)
+    special = random.choices("!@#$%&*-+=?", k=2)
+
+    #plusser alt sammen
+    password_list = upper + lower + digits + special
+    # Bland rekkefølgen tilfeldig
+    random.shuffle(password_list)
+    # limer sammen alt i pasword_list til en streng
+    return "".join(password_list)
+    
+    
 
 # Main program starter her
 password = input("Skriv inn et passord: ")
@@ -12,6 +25,6 @@ password = input("Skriv inn et passord: ")
 # Kall funksjonen for å hashe passordet
 hashed_password = hash_password(password)
 
-# Output
+#printer ut passordet og den hashet versionen
 print("Originalt passord:", password)
 print("Hashet passord:", hashed_password)
