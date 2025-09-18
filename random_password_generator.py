b49ba2d54e0ae3f1b2618fae519510e72f49dee8
egvetikkjekaegskalhete <gabrielaasen@gmail.com>
2025-09-18 12:36:33 +0200

    Skrevet ferdig koden så nå generer den ett pasword etter kravene!

--- random_password_generator.py
+++ random_password_generator.py
@@ -8,13 +8,24 @@ def shuffle(string):
   return ''.join(tempList)
 
 #Main program starts here
-uppercaseLetter1=chr(random.randint(65,90)) #Generate a random Uppercase letter (based on ASCII code)
-uppercaseLetter2=chr(random.randint(65,90)) #Generate a random Uppercase letter (based on ASCII code)
-#Generate more characters here
-#....
+uppercaseLetter1 = chr(random.randint(65, 90))  # Store bokstaver
+uppercaseLetter2 = chr(random.randint(65, 90))
+
+lowercaseLetter1 = chr(random.randint(97, 122))  # Små bokstaver
+lowercaseLetter2 = chr(random.randint(97, 122))
+
+digit1 = chr(random.randint(48, 57))  # Sifre
+digit2 = chr(random.randint(48, 57))
+
+special_chars = "!#$@?-*+="
+special1 = random.choice(special_chars)  # Spesialtegn
+special2 = random.choice(special_chars)
+
+
 
 #Generate password using all the characters, in random order
-password = uppercaseLetter1 + uppercaseLetter2 # + ....
+password = uppercaseLetter1 + uppercaseLetter2 + lowercaseLetter1 + lowercaseLetter2 + digit1 + digit2 + special1 + special2
+
 password = shuffle(password)
 
 #Ouput
